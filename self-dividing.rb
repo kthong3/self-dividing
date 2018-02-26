f2d679a5d7df0fb63e3e2eb8ea99b8d3269acdad
kthong3 <kthongsakounh@gmail.com>
2018-02-26 15:50:04 -0600

    remove numbers that include 0 from range since it is not divisible

--- self-dividing.rb
+++ self-dividing.rb
@@ -6,6 +6,7 @@
 # create output array
 # convert range into array of integers
 # iterate through array
+  # discard any number that includes 0 since numbers cannot divide by 0
   # count how many digits
   # check if integer is divisible by each digit
   # skip if not divisible
@@ -13,17 +14,21 @@
 # return output array
 
 def is_self_dividing?(upper, lower)
-  range = (upper.to_s..lower.to_s).to_a
+  range_str = (upper.to_s..lower.to_s).to_a
   output = []
 
-  range.each do |number|
+  range_str.delete_if {|input| input.include?("0") }
+  p range_str
+
+  range_str.each do |number|
     digits = number.split("")
-    p digits
+
     digits.each do |digit|
-      p digit
+      if number.to_i % digit.to_i == 0
+        p digit
+      end
     end
   end
-
 end
 
-is_self_dividing?(1,10)
+is_self_dividing?(1,12)
