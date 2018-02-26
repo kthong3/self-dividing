a92a873b36f3c6c7d4d92a0c11773dfc105add56
kthong3 <kthongsakounh@gmail.com>
2018-02-26 14:55:11 -0600

    add pseudocode

---
+++ self-dividing.rb
@@ -0,0 +1,17 @@
+# pseudocode
+# input: upper bound integer and lower bound integer
+# output: array with self-dividing integers
+
+# create range with upper and lower
+# create output array
+# convert range into array of integers
+# iterate through array
+  # count how many digits
+  # check if integer is divisible by each digit
+  # skip if not divisible
+  # add integer to output array if divisible by all digits
+# return output array
+
+def is_self_dividing?(upper, lower)
+
+end
